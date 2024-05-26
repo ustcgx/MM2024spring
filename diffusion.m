5a6e7e806e2b09a8066a5ec491a3aa7d24faf8cb
Xu Guo <gx_21_nlfd@mail.ustc.edu.cn>
2024-05-26 16:57:52 +0800

    first commit

--- diffusion.m
+++ diffusion.m
@@ -18,7 +18,9 @@ dz = 1; % z方向网格大小 m
 dt = 0.1; % 时间步长 s
 
 D = 1; % 扩散系数 m^2/s，取相对大值
+alpha = 1.0 % 反射系数，1.0代表全反射，0代表全吸收
 v = 1; % 风速，沿x轴方向 m/s
+w = 0.006; % 模拟烟尘重力的风速，沿z轴负向 m/s
 C0 = 1e-2; % 烟囱排放的恒定浓度 kg/s
 h = 20; % 烟囱高度 m
 
@@ -49,7 +51,8 @@ for t = 0:dt:T
                 C_new(i,j,k) = C(i,j,k) + dt * D * ((C(i+1,j,k) - 2*C(i,j,k) + C(i-1,j,k)) / dx^2 ...
                     + (C(i,j+1,k) - 2*C(i,j,k) + C(i,j-1,k)) / dy^2 ...
                     + (C(i,j,k+1) - 2*C(i,j,k) + C(i,j,k-1)) / dz^2) ...
-                    - dt * v * (C(i+1,j,k) - C(i-1,j,k)) / (2*dx);
+                    - dt * v * (C(i+1,j,k) - C(i-1,j,k)) / (2*dx) ...
+                    - dt * w * (C(i,j,k+1) - C(i,j,k-1)) / (2*dz);
                 if (C_new(i,j,k)<0)
                     C_new(i,j,k) = 0;
                 end
@@ -60,13 +63,13 @@ for t = 0:dt:T
     %% 边界条件
     C_new(center, center, height) = C_new(center, center, height) + dt * C0;
 
-    % 应用地面反射边界条件（零通量）假设地面反射后浓度不变
-    C_new(:, :, 1) = C(:, :, 2);
+    % 应用地面反射边界条件
+    C_new(:, :, 1) = alpha * C(:, :, 2);
 
     %% 监测变量
-    C_near = C_new(center-1:center+1, center-1:center+1, height)
-    total_delta_kg = sum(C_new-C,"all")
-    max_delta_kg = max(C_new-C, [], "all")
+    C_near = C_new(center-1:center+1, center-1:center+1, height);
+    total_delta_kg = sum(C_new-C,"all");
+    max_delta_kg = max(C_new-C, [], "all");
     
     % 更新浓度分布
     C = C_new;
